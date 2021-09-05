082d22d33d8eafc36716310ee0bdde1053770a77
Reshani Mihirika <reshaniyva@gmail.com>
2021-09-06 08:43:21 +1000

    [UI]:Completed Intro pages layout (#70)

--- app/src/main/java/com/e/uvsafeaustralia/SlideViewPagerAdapter.java
+++ app/src/main/java/com/e/uvsafeaustralia/SlideViewPagerAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.QuickContactBadge;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
@@ -22,7 +21,7 @@ public class SlideViewPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return 5;
     }
 
     @Override
@@ -37,9 +36,11 @@ public class SlideViewPagerAdapter extends PagerAdapter {
         View view = inflater.inflate(R.layout.activity_slide_pages, container, false);
 
         ImageView introInfo1 = view.findViewById(R.id.imageViewIntroInfo1);
-        ImageView sliderRadio1 = view.findViewById(R.id.imageViewSliderRadio1);
-        ImageView sliderRadio2 = view.findViewById(R.id.imageViewSliderRadio2);
-        ImageView sliderRadio3 = view.findViewById(R.id.imageViewSliderRadio3);
+        ImageView sliderRadio1 = view.findViewById(R.id.sliderRadio1);
+        ImageView sliderRadio2 = view.findViewById(R.id.sliderRadio2);
+        ImageView sliderRadio3 = view.findViewById(R.id.sliderRadio3);
+        ImageView sliderRadio4 = view.findViewById(R.id.sliderRadio4);
+        ImageView sliderRadio5 = view.findViewById(R.id.sliderRadio5);
         ImageView nextBtn = view.findViewById(R.id.imageViewNextIcn2);
         ImageView backBtn = view.findViewById(R.id.imageViewBackIcn1);
         Button getStartedBtn = view.findViewById(R.id.getStartedBtn);
@@ -75,28 +76,56 @@ public class SlideViewPagerAdapter extends PagerAdapter {
 
         switch(position){
             case 0:
-                introInfo1.setImageResource(R.drawable.slide1);
+                introInfo1.setImageResource(R.drawable.slider1);
                 sliderRadio1.setImageResource(R.drawable.ic_slider_selected);
                 sliderRadio2.setImageResource(R.drawable.ic_slider_not_selected);
                 sliderRadio3.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio4.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio5.setImageResource(R.drawable.ic_slider_not_selected);
                 backBtn.setVisibility(View.GONE);
                 nextBtn.setVisibility(View.VISIBLE);
                 break;
 
             case 1:
-                introInfo1.setImageResource(R.drawable.slide2);
+                introInfo1.setImageResource(R.drawable.slider2);
                 sliderRadio1.setImageResource(R.drawable.ic_slider_not_selected);
                 sliderRadio2.setImageResource(R.drawable.ic_slider_selected);
                 sliderRadio3.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio4.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio5.setImageResource(R.drawable.ic_slider_not_selected);
                 backBtn.setVisibility(View.VISIBLE);
                 nextBtn.setVisibility(View.VISIBLE);
                 break;
 
             case 2:
-                introInfo1.setImageResource(R.drawable.slide3);
+                introInfo1.setImageResource(R.drawable.slider3);
                 sliderRadio1.setImageResource(R.drawable.ic_slider_not_selected);
                 sliderRadio2.setImageResource(R.drawable.ic_slider_not_selected);
                 sliderRadio3.setImageResource(R.drawable.ic_slider_selected);
+                sliderRadio4.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio5.setImageResource(R.drawable.ic_slider_not_selected);
+                backBtn.setVisibility(View.VISIBLE);
+                nextBtn.setVisibility(View.VISIBLE);
+                break;
+
+            case 3:
+                introInfo1.setImageResource(R.drawable.slider4);
+                sliderRadio1.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio2.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio3.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio4.setImageResource(R.drawable.ic_slider_selected);
+                sliderRadio5.setImageResource(R.drawable.ic_slider_not_selected);
+                backBtn.setVisibility(View.VISIBLE);
+                nextBtn.setVisibility(View.VISIBLE);
+                break;
+
+            case 4:
+                introInfo1.setImageResource(R.drawable.slider5);
+                sliderRadio1.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio2.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio3.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio4.setImageResource(R.drawable.ic_slider_not_selected);
+                sliderRadio5.setImageResource(R.drawable.ic_slider_selected);
                 backBtn.setVisibility(View.VISIBLE);
                 nextBtn.setVisibility(View.GONE);
                 break;
