77af73141cdc836875433affbde43d25e26af3b9
Maya Treacy <mayatreacy@gmail.com>
2021-09-10 08:42:12 +1000

    [UI]: Fix bug on Protection Measures back button (#122)

--- app/src/main/java/com/e/uvsafeaustralia/ProtectionActivity.java
+++ app/src/main/java/com/e/uvsafeaustralia/ProtectionActivity.java
@@ -1,7 +1,10 @@
 package com.e.uvsafeaustralia;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProtectionActivity extends AppCompatActivity {
@@ -9,5 +12,18 @@ public class ProtectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protection_measures);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
