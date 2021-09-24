9df19f0df94a40b1cd9670b719a86847ba420b4d
Shuang Xu <89244345+sxuu0044@users.noreply.github.com>
2021-09-24 08:53:34 +0800

    [FEAT] add about melanoma video2 (#217)

--- app/src/main/java/com/e/uvsafeaustralia/views/AboutMelanomaActivity.java
+++ app/src/main/java/com/e/uvsafeaustralia/views/AboutMelanomaActivity.java
@@ -2,20 +2,36 @@ package com.e.uvsafeaustralia.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 
+import com.e.uvsafeaustralia.R;
 import com.e.uvsafeaustralia.databinding.ActivityAboutMelanomaBinding;
 import com.e.uvsafeaustralia.databinding.ActivityQuizFourBlocksBinding;
 
 public class AboutMelanomaActivity extends AppCompatActivity {
 
     private ActivityAboutMelanomaBinding binding;
+    private MediaController mediaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAboutMelanomaBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        mediaController = new MediaController(this);
+        Uri uri = Uri.parse("android.resource://com.e.uvsafeaustralia/"+ R.raw.aboutmelonoma2);
+        binding.aboutMelanomaVideo2.setVideoURI(uri);
+        binding.aboutMelanomaVideo2.setMediaController(mediaController);
+        mediaController.setAnchorView(binding.aboutMelanomaVideo1);
+    }
+    public void playAboutMelanomaVideo1() {
+        Uri uri = Uri.parse("android.resource://com.e.uvsafeaustralia/"+ R.raw.aboutsunscreen);
+        binding.aboutMelanomaVideo1.setVideoURI(uri);
+        binding.aboutMelanomaVideo1.setMediaController(mediaController);
+        mediaController.setAnchorView(binding.aboutMelanomaVideo1);
+        binding.aboutMelanomaVideo1.start();
     }
 }
