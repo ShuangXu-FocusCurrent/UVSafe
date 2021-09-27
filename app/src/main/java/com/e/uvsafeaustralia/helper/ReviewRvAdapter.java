5dc5d4e228c3c8466fd9acd58100d7a3baf88d55
Shuang Xu <89244345+sxuu0044@users.noreply.github.com>
2021-09-27 17:14:38 +0800

    [FEAT] Add Recycle view for the category review1 (#257)
    * [FEAT] Add Recycle view for the category review1
    
    * [FEAT] Add Recycle view for the category review1

---
+++ app/src/main/java/com/e/uvsafeaustralia/helper/ReviewRvAdapter.java
@@ -0,0 +1,68 @@
+package com.e.uvsafeaustralia.helper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.e.uvsafeaustralia.R;
+import com.e.uvsafeaustralia.databinding.LeaderboardRvLayoutBinding;
+import com.e.uvsafeaustralia.databinding.OnelineReviewRvLayoutBinding;
+import com.e.uvsafeaustralia.models.AnswerModel;
+import com.e.uvsafeaustralia.models.LeaderboardModel;
+
+import java.util.List;
+
+public class ReviewRvAdapter extends RecyclerView.Adapter
+        <ReviewRvAdapter.ViewHolder> {
+
+    List<AnswerModel> models;
+    Context context;
+    public ReviewRvAdapter(List<AnswerModel> models, Context context){
+        this.models = models;
+        this.context = context;
+    }
+
+    @Override
+    public ReviewRvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        OnelineReviewRvLayoutBinding binding = OnelineReviewRvLayoutBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ReviewRvAdapter.ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewRvAdapter.ViewHolder holder, int position) {
+        final AnswerModel model = models.get(position);
+
+        holder.binding.quetionNumber.setText(String.valueOf(model.getQuestion().getqId()));
+        holder.binding.question.setText(model.getQuestion().getQuestion());
+        holder.binding.option1.setText(model.getQuestion().getAnswerOption1());
+        holder.binding.option2.setText(model.getQuestion().getAnswerOption2());
+        holder.binding.option3.setText(model.getQuestion().getAnswerOption3());
+        holder.binding.option4.setText(model.getQuestion().getAnswerOption4());
+        holder.binding.explain.setText(model.getQuestion().getAnswerExplain());
+        if(model.getStatus()==1){
+            holder.binding.status.setImageResource(R.drawable.right);
+        }else{
+            holder.binding.status.setImageResource(R.drawable.wrong);
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return models.size() ;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private OnelineReviewRvLayoutBinding binding;
+        public ViewHolder(OnelineReviewRvLayoutBinding binding){
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/e/uvsafeaustralia/views/quiz/reportWithReview/Category1ReviewActivity.java
+++ app/src/main/java/com/e/uvsafeaustralia/views/quiz/reportWithReview/Category1ReviewActivity.java
@@ -1,16 +1,32 @@
 package com.e.uvsafeaustralia.views.quiz.reportWithReview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.e.uvsafeaustralia.databinding.ActivityCategory1ReviewBinding;
+import com.e.uvsafeaustralia.helper.ReviewRvAdapter;
+import com.e.uvsafeaustralia.models.AnswerModel;
+import com.e.uvsafeaustralia.models.QuestionModel;
+import com.e.uvsafeaustralia.models.UserModel;
 import com.e.uvsafeaustralia.views.quiz.QuizFourBlocksActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.e.uvsafeaustralia.models.QuestionModel.EnumQCategory.CATEGORY1;
 
 public class Category1ReviewActivity extends AppCompatActivity {
     private ActivityCategory1ReviewBinding binding;
+    private RecyclerView.LayoutManager layoutManager;
+    private ReviewRvAdapter adapter;
+    private List<AnswerModel> models;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,17 @@ public class Category1ReviewActivity extends AppCompatActivity {
         binding = ActivityCategory1ReviewBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        models = new ArrayList<AnswerModel>();
+        UserModel user = new UserModel(1,"Jade");
+        QuestionModel question = new QuestionModel(1,CATEGORY1,1,"I'm a dangerous sun what could be my UV level?","Below3","3 or above","null","null","3 or above","The correct answer is b.");
+        AnswerModel model = new AnswerModel(1,user,question,"a",1);
+        models.add(model);
+        adapter = new ReviewRvAdapter(models, Category1ReviewActivity.this);
+        binding.reviewRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        binding.reviewRecyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this);
+        binding.reviewRecyclerView.setLayoutManager(layoutManager);
 
         binding.backMainQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
